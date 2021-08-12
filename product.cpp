244228518f2cfb6dc5ad5d28d5c6713558c81746
AshisKumarPadhy <86865655+AshisKumarPadhy@users.noreply.github.com>
2021-08-12 21:48:52 +0530

    Add files via upload

---
+++ product.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+ int main() 
+ { double a,b,mul;
+ cout<<"Enter Two numbers: ";
+    cin>>a>>b;
+    mul=a*b;
+    cout<<"mul = "<<mul;
+    return 0;
+     
+ }
