b4fb3aa455be8446d384109d96871fc437057e67
AshisKumarPadhy <86865655+AshisKumarPadhy@users.noreply.github.com>
2021-08-13 10:23:37 +0530

    Add files via upload

---
+++ Voting.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int a;
+   cout<<"enter the age :";
+   cin>>a;
+   if (a<18)
+   {
+       cout<<"you are not eligible";
+       
+   }
+   else {
+       cout<<"\n You are eligible";
+   }
+    return 0;
+}
